a9110304e941fc76ed713a3612bdd5fca3dfe01f
unknown <yukkeaa@gmail.com>
2026-04-24 20:53:52 +0900

    Auto update: 20260424-2053

--- src/lexer.py
+++ src/lexer.py
@@ -113,8 +113,6 @@ def tokenize(source: str) -> list[Token]:
             raise RuntimeError(f"予想外のトークン！{mo.group()}")
         if kind == 'SKIP' or kind == 'COMMENT':
             continue
-        if kind == 'TYPE_MUT':
-            tokens.append(Token(TokenType.tBORROW, value, line, col, value))
         elif kind == 'TYPE_REF':
             tokens.append(Token(TokenType.BACKQUOTE, value, line, col, value))
         elif kind == "UNSAFE":
