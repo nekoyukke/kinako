d03f3574f5c6f3233f2e90f77deffbbbe5e77bc6
nekoyukke <yukkeaa@gmail.com>
2026-04-28 03:08:14 +0900

    Test

--- gitsync.ps1
+++ gitsync.ps1
@@ -1,4 +1,4 @@
-$now = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+$now = Get-Date -Format "yyyyMMdd-HHmm"
 git switch -c "branch-$now"
 git commit --allow-empty -m "新しいブランチ作成: $(Get-Date -Format 'yyyy/MM/dd HH:mm:ss')"
 git push kinako "branch-$now"
