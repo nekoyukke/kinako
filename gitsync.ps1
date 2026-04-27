747c49e7466f48abf60042a38b981f62b8ae70da
nekoyukke <yukkeaa@gmail.com>
2026-04-28 03:30:56 +0900

    tests

--- gitsync.ps1
+++ gitsync.ps1
@@ -6,9 +6,8 @@ git add .
 git commit -m $args[0]
 git push kinako "branch-$now"
 git switch main
-# 戻ったとき、もし「作業中の残骸」があれば消して真っさらにする
-git checkout .  # ← 変更を全部捨てて、最後に成功したコミットの状態に戻す（※注意：未保存は消えます）
-# GitHub上の最新（合体後のmain）を取り込んで、常に自分を最新にする
+Read-Host "エンターキーを押すと次へ進みます..."
+git checkout .
 git pull kinako main --rebase
 echo "remote:"
 git remote
