0fc139366c2c7059a26a51c229618db1b13a09a2
unknown <yukkeaa@gmail.com>
2026-05-31 14:06:43 +0900

    a

--- src/core/id_base/scope_id.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class ScopeId:
-    value: int
-
-    
-    def __str__(self) -> str:
-        return f"scope_{self.value}"
--- src/core/state/state.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class State:
-    initialized: bool
-    locked: bool
